a89c543e97427dac664923394f4cf9dc3bc38823
Kevin Lacker <lacker@gmail.com>
2021-01-22 13:20:09 -0800

    libp2p basic

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
 console.log("hello world");
+
+let Libp2p = require("libp2p");
+let WebSockets = require("libp2p-websockets");
+import { NOISE } from "libp2p-noise";
+
+async function main() {
+  let node = await Libp2p.create({
+    modules: {
+      connEncryption: [NOISE],
+      transport: [WebSockets]
+    }
+  });
+}
+
+main();
