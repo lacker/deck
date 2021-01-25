b324b2f1697462da588f8eac1fd335eae74f4025
Kevin Lacker <lacker@gmail.com>
2021-01-25 09:14:40 -0800

    switch to tcp

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 console.log("hello world");
 
 let Libp2p = require("libp2p");
-let WebSockets = require("libp2p-websockets");
+let TCP = require("libp2p-tcp");
 import { NOISE } from "libp2p-noise";
 
 async function main() {
   let node = await Libp2p.create({
     modules: {
       connEncryption: [NOISE],
-      transport: [WebSockets]
+      transport: [TCP]
     }
   });
 }
