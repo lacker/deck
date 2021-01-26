0c8dc084c3e20a029db3f2ebe645f45802f38df5
Kevin Lacker <lacker@gmail.com>
2021-01-26 13:49:29 -0800

    trying bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ let TCP = require("libp2p-tcp");
 import { NOISE } from "libp2p-noise";
 let MPLEX = require("libp2p-mplex");
 let multiaddr = require("multiaddr");
+let Bootstrap = require("libp2p-bootstrap");
+
+let BOOTSTRAP_MULTIADDRS = [
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN"
+];
+
+function sleep(seconds: number) {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000));
+}
 
 async function main() {
   let node = await Libp2p.create({
@@ -11,22 +21,43 @@ async function main() {
     },
     modules: {
       connEncryption: [NOISE],
-      transport: [TCP]
+      transport: [TCP],
+      streamMuxer: [MPLEX],
+      peerDiscovery: [Bootstrap]
+    },
+    config: {
+      peerDiscovery: {
+        autoDial: true,
+        [Bootstrap.tag]: {
+          enabled: true,
+          list: BOOTSTRAP_MULTIADDRS
+        }
+      }
     }
+  });
+
+  node.on("peer:discovery", (peer: any) => {
+    console.log("Discovered", peer.id.toB58String());
+  });
+
+  node.connectionManager.on("peer:connect", (connection: any) => {
+    console.log("Connected to", connection.remotePeer.toB58String());
   });
 
   await node.start();
   console.log("libp2p started");
 
-  console.log("listening");
   node.multiaddrs.forEach((addr: any) => {
-    console.log(`${addr.toString()}/p2p/${node.peerId.toB58String()}`);
+    console.log(
+      `listening on ${addr.toString()}/p2p/${node.peerId.toB58String()}`
+    );
   });
 
+  await sleep(10);
   await node.stop();
   console.log("stopped");
+  process.exit();
 }
 
 console.log("running main");
 main();
-console.log("eof");
