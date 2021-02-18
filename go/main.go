d52a9371788da2321225b4dd8759e9edd7a8fcc8
Kevin Lacker <lacker@gmail.com>
2021-02-18 09:36:24 -0800

    bootstrapping

--- go/main.go
+++ go/main.go
@@ -14,7 +14,7 @@ func main() {
 	ctx := context.Background()
 
 	// start a libp2p node listening on port 2000
-	node, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2000"))
+	host, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2000"))
 	if err != nil {
 		panic(err)
 	}
@@ -22,10 +22,21 @@ func main() {
 	fmt.Println("not really using:", dht.DefaultBootstrapPeers)
 
 	// print the node's listening addresses
-	fmt.Println("Listening on addresses:", node.Addrs())
+	fmt.Println("Listening on addresses:", host.Addrs())
+
+	// Start a DHT
+	kad, err := dht.New(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+
+	// Bootstrap it
+	if err = kad.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
 
 	// shut the node down
-	if err := node.Close(); err != nil {
+	if err := host.Close(); err != nil {
 		panic(err)
 	}
 }
