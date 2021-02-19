c198ef387451e5ec59a614e523b689eaec2c7bfe
Kevin Lacker <lacker@gmail.com>
2021-02-19 13:26:08 -0800

    works

--- go/go.mod
+++ go/go.mod
@@ -3,7 +3,11 @@ module github.com/lacker/deck
 go 1.15
 
 require (
+	github.com/google/go-cmp v0.5.4 // indirect
 	github.com/libp2p/go-libp2p v0.13.0
-	github.com/libp2p/go-libp2p-kad-dht v0.11.1 // indirect
-	golang.org/x/tools/gopls v0.6.5 // indirect
+	github.com/libp2p/go-libp2p-core v0.8.0
+	github.com/libp2p/go-libp2p-kad-dht v0.11.1
+	golang.org/x/mod v0.4.0 // indirect
+	golang.org/x/tools v0.1.1-0.20210201201750-4d4ee958a9b7 // indirect
+	honnef.co/go/tools v0.0.1-2020.1.6 // indirect
 )
--- go/main.go
+++ go/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/peer"
 
 	dht "github.com/libp2p/go-libp2p-kad-dht"
 )
@@ -34,6 +36,23 @@ func main() {
 	if err = kad.Bootstrap(ctx); err != nil {
 		panic(err)
 	}
+
+	// Let's connect to the bootstrap nodes first. They will tell us about the
+	// other nodes in the network.
+	var wg sync.WaitGroup
+	for _, peerAddr := range dht.DefaultBootstrapPeers {
+		peerinfo, _ := peer.AddrInfoFromP2pAddr(peerAddr)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if err := host.Connect(ctx, *peerinfo); err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Println("Connection established with bootstrap node:", *peerinfo)
+			}
+		}()
+	}
+	wg.Wait()
 
 	// shut the node down
 	if err := host.Close(); err != nil {
