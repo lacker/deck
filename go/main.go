7a4e942f73e89340a3134148497aaeefbe2878b3
Kevin Lacker <lacker@gmail.com>
2021-03-10 09:07:55 -0800

    rando mport it

--- go/main.go
+++ go/main.go
@@ -41,8 +41,8 @@ func main() {
 	// create a background context (i.e. one that never cancels)
 	ctx := context.Background()
 
-	// start a libp2p node listening on port 2000
-	host, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/2000"))
+	// start a libp2p node listening on a random port
+	host, err := libp2p.New(ctx, libp2p.ListenAddrStrings("/ip4/127.0.0.1/tcp/0"))
 	if err != nil {
 		panic(err)
 	}
